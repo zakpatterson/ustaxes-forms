ed98cc4acf1005e71dea55761817050e0b559c8e
Zak Patterson <pattersonzak@gmail.com>
2021-12-01 21:34:24 +0900

    Fix two lingering changes caught by tsc

--- src/tests/ScheduleD.test.ts
+++ src/tests/ScheduleD.test.ts
@@ -2,9 +2,9 @@ import * as fc from 'fast-check'
 import { with1040Property } from './common/F1040'
 
 describe('ScheduleD', () => {
-  it('should never pass through more than allowed losses', () => {
-    fc.assert(
-      with1040Property(([f1040]): void => {
+  it('should never pass through more than allowed losses', async () => {
+    await fc.assert(
+      with1040Property(async ([f1040]): Promise<void> => {
         expect(Math.round(f1040.l7() ?? 0)).toBeGreaterThanOrEqual(
           -(f1040.scheduleD?.l21Min() ?? Number.POSITIVE_INFINITY)
         )
