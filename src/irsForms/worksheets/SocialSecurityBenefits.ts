ffad387f005d05c565a2c3dfe6dd905b8de4275a
Zak Patterson <pattersonzak@gmail.com>
2021-12-01 21:34:24 +0900

    Fix two lingering changes caught by tsc

--- src/irsForms/worksheets/SocialSecurityBenefits.ts
+++ src/irsForms/worksheets/SocialSecurityBenefits.ts
@@ -53,7 +53,7 @@ export default class SocialSecurityBenefitsWorksheet {
    */
   l6 = (): number =>
     sumFields([
-      this.f1040.l10b(),
+      this.f1040.l12b(),
       this.f1040.schedule1?.l10(),
       this.f1040.schedule1?.l11(),
       this.f1040.schedule1?.l12(),
@@ -63,8 +63,8 @@ export default class SocialSecurityBenefitsWorksheet {
       this.f1040.schedule1?.l16(),
       this.f1040.schedule1?.l17(),
       this.f1040.schedule1?.l18(),
-      this.f1040.schedule1?.l19(),
-      this.f1040.schedule1?.l22writeIn()
+      this.f1040.schedule1?.l19a(),
+      this.f1040.schedule1?.l20()
     ])
 
   /* Line 7: Is the amount on line 6 less than the amount on line 5?
