0c18a0f91562254b1d768e172c3c89e57ce050cf
Zak Patterson <pattersonzak@gmail.com>
2021-12-03 15:08:03 +0900

    Backport 2020 updates

--- src/tests/pdfFiller/pdfHandler.test.ts
+++ src/tests/pdfFiller/pdfHandler.test.ts
@@ -1,9 +1,10 @@
 import * as fc from 'fast-check'
-import { PDFField, PDFTextField } from 'pdf-lib'
+import { PDFField, PDFTextField, PDFDocument } from 'pdf-lib'
 import { create1040PDFs } from '../../irsForms'
 import * as arbitraries from '../../tests/arbitraries'
 import { Information } from '../../data'
 import { localPDFs } from '../../tests/common/LocalForms'
+import { with1040Pdfs } from '../common/F1040'
 
 jest.setTimeout(120 * 1000)
 
@@ -13,63 +14,83 @@ beforeAll(() => {
   jest.spyOn(console, 'warn').mockImplementation(() => {})
 })
 
+
 const findBadDecimalFormat = (
-  field: PDFField
+  field: PDFField,
+  places = 2
 ): [string, string] | undefined => {
   if (field instanceof PDFTextField) {
     const text = field.getText()
     if (text !== undefined && !isNaN(Number(text))) {
       const numberParts = text.split('.')
-      if (numberParts.length === 2 && numberParts[1].length !== 2) {
+      if (numberParts.length === 2 && numberParts[1].length !== places) {
         return [field.getName(), text]
       }
     }
   }
 }
 
-/**
- * Run a test for a given return, and report on [pdf title, [fieldName, testResult]] for
- * each field that has a malformatted number.
- */
-const testEveryField = async <A>(
-  info: Information,
-  fieldMatch: (field: PDFField) => A | undefined
-): Promise<[string, [number, A][]][]> =>
-  create1040PDFs(info)(localPDFs)
-    .then<[string, [number, A][]][]>((schedules) =>
-      schedules.flatMap((pdf, i) => {
-        const pdfBadFields: [number, A][] = pdf
+describe('pdfHandler', () => {
+  it('every field has only two decimal places max', async () => {
+    await with1040Pdfs((pdfs) => {
+      type FieldError = [number, [string, string]]
+      type PdfErrors = [string, FieldError[]]
+
+      // Specific fields that have different rounding instructions:
+      const exceptionFields: [string, number, number][] = [
+        ['2021 Schedule 8812 (Form 1040)', 66, 3]
+      ]
+
+      const badFields: PdfErrors[] = pdfs.flatMap((pdf, i): PdfErrors[] => {
+        const pdfBadFields: FieldError[] = pdf
           .getForm()
           .getFields()
           .flatMap((field, i) => {
-            const fieldValue = fieldMatch(field)
+            const places =
+              exceptionFields.find(
+                ([name, index]) => name === pdf.getTitle() && index === i
+              )?.[2] ?? 2
+
+            const fieldValue = findBadDecimalFormat(field, places)
             if (fieldValue !== undefined) {
-              return [[i, fieldValue]]
+              return [[i, fieldValue]] as FieldError[]
             }
-            return []
+            return [] as FieldError[]
           })
 
         if (pdfBadFields.length > 0) {
           return [[pdf.getTitle() ?? `pdf-${i}`, pdfBadFields]]
         } else {
-          return []
+          return [] as PdfErrors[]
         }
       })
-    )
-    .catch(() => {
-      // ignore cases where 1040 cannot be generated
-      return []
+      expect(badFields).toEqual([])
     })
+  })
 
-describe('pdfHandler', () => {
-  it('every field has only two decimal places max', async () => {
-    await fc.assert(
-      fc.asyncProperty(arbitraries.information, async (info) => {
-        expect(await testEveryField(info, findBadDecimalFormat)).toHaveLength(0)
-      }),
-      {
-        interruptAfterTimeLimit: 100 * 1000 // 100 seconds
+  it('taxpayer name found in header of all forms', async () => {
+    await with1040Pdfs((pdfs, info) => {
+      if (pdfs.length === 0) {
+        // Ingore cases where info led to no forms
+        return true
       }
-    )
+
+      const firstNameNotInHeader: PDFDocument[] = pdfs.filter(
+        (pdf) =>
+          !pdf
+            .getForm()
+            .getFields()
+            .slice(0, 20)
+            .some(
+              (field) =>
+                field instanceof PDFTextField &&
+                field
+                  .getText()
+                  ?.startsWith(info.taxPayer.primaryPerson?.firstName ?? '')
+            )
+      )
+
+      expect(firstNameNotInHeader).toEqual([])
+    })
   })
 })
